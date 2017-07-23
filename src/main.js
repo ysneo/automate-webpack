1b6ab7239ddb9d916318192e89f038cdc8389d6d
ysneo <jinchn728@gmail.com>
2017-07-23 19:06:11 +0800

    prepare to start webpack

---
+++ src/main.js
@@ -0,0 +1,4 @@
+const add = require('./child_a');
+
+const result = add(133, 2);
+console.log(result);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    entry: './app/index.js',
+    entry: './src/main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
